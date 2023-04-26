69230cd64d8cc81eff6f541acc78ea6906231728
John Victor <johnvcodes@proton.me>
2023-04-26 19:52:11 -0300

    Initial Commit.

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
