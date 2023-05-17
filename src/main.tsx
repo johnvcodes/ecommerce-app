35052893320f16baeb8e5c4b3770b8264aae7441
John Victor <johnvcodes@proton.me>
2023-05-17 13:07:11 -0300

    Add routing.

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,20 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+import Products from "./pages/Products";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<CreateAccount />} />
+      <Route path="/products" element={<Products />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
