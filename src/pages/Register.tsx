82b96cd2b867453fd45338d8c5e8c5fc3da6856c
John Victor <johnvcodes@proton.me>
2023-05-20 13:09:49 -0300

    Add routing and error handling.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,9 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ChangeEvent, FormEvent, useReducer, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
 import { auth } from "../firebase/firebaseConfig";
+import getErrorMessage from "../utilities/get-error-message";
 
 type RegisterState = {
   email: string;
@@ -27,7 +28,7 @@ const registerReducer = (
 ): RegisterState => {
   switch (action.type) {
     case "clear":
-      return { ...state };
+      return { email: "", password: "", confirmPassword: "" };
     case "email":
       if (!action.payload) return state;
       return { ...state, email: action.payload };
@@ -44,6 +45,7 @@ const registerReducer = (
 };
 
 function Register() {
+  const navigate = useNavigate();
   const [{ email, password, confirmPassword }, registerDispatch] = useReducer(
     registerReducer,
     registerValue
@@ -53,7 +55,7 @@ function Register() {
 
   const handleRegisterInput = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    registerDispatch({
+    return registerDispatch({
       type: name as keyof RegisterState,
       payload: value,
     });
@@ -72,11 +74,10 @@ function Register() {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
     } catch (error) {
-      if (error instanceof FirebaseError)
-        return setRegisterError(error.message);
-      return String(error);
+      return setRegisterError(getErrorMessage(error));
     }
-    return registerDispatch({ type: "clear" });
+    registerDispatch({ type: "clear" });
+    return navigate("/");
   };
 
   return (
@@ -102,7 +103,7 @@ function Register() {
         name="email"
         id="email"
         placeholder="Ex: meu@email.com"
-        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-900"
+        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow outline-none transition-colors duration-300 placeholder:text-neutral-500 hover:border-neutral-500 focus:border-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:border-neutral-500 dark:focus:border-neutral-500"
       />
       <label htmlFor="password" className="w-fit font-bold">
         Senha
@@ -114,7 +115,7 @@ function Register() {
         name="password"
         id="password"
         placeholder="Mínimo de 6 caractéres"
-        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-900"
+        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow outline-none transition-colors duration-300 placeholder:text-neutral-500 hover:border-neutral-500 focus:border-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:border-neutral-500 dark:focus:border-neutral-500"
       />
       <label htmlFor="confirm-password" className="w-fit font-bold">
         Confirmar senha
@@ -126,11 +127,11 @@ function Register() {
         name="confirmPassword"
         id="confirm-password"
         placeholder="Mínimo de 6 caractéres"
-        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-900"
+        className="mb-2 flex items-center rounded border border-neutral-300 bg-neutral-50 p-4 shadow outline-none transition-colors duration-300 placeholder:text-neutral-500 hover:border-neutral-500 focus:border-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:border-neutral-500 dark:focus:border-neutral-500"
       />
       <button
         type="submit"
-        className="mb-2 flex items-center self-center rounded bg-blue-700 px-4 py-2 font-bold uppercase text-neutral-50 transition-colors duration-300 hover:bg-blue-500"
+        className="mb-2 flex items-center self-center rounded bg-blue-700 px-4 py-2 font-bold uppercase text-neutral-50 shadow transition-colors duration-300 hover:bg-blue-500"
       >
         Confirmar
       </button>
@@ -138,7 +139,7 @@ function Register() {
         Já possui uma conta?
         <Link
           to="/login"
-          className="font-bold text-blue-700 transition-colors duration-300 hover:text-blue-500"
+          className="font-bold text-blue-700 transition-colors duration-300 after:block after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-blue-500 after:transition-transform after:duration-300 hover:text-blue-500 hover:after:scale-x-100"
         >
           Entrar
         </Link>
